85fb25759c738043654601cd850867c7efa38544
金健翔 <andy_ios@163.com>
2014-09-14 14:06:02 +0800

    基本完成聊天室

--- node_chat/chat_service/routes/users.js
+++ node_chat/chat_service/routes/users.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mqtt = require('mqtt')
 
 /* GET users listing. */
 router.get('/', function(req, res) {
+	
+	// 解决跨域问题
+  res.header("Access-Control-Allow-Origin", "*");   
 
 	var topic = req.param('topic');
 	var payload = req.param('payload');
-	console.log(topic);
-	console.log(payload);
-
-  res.send('respond with a resource');
+	
+	sendMsg(res, topic, payload);
 });
 
+function sendMsg(res, topic, payload) {
+	client = mqtt.createClient(1883, '192.168.1.11');
+
+	client.publish(topic, payload);
+
+	client.on('message', function (topic, message) {
+		res.send(topic + ' say: ' + message);
+	});
+
+	client.end();
+}
+
 module.exports = router;
+
